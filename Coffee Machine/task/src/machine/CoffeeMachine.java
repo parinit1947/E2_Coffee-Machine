7d8a007868dc29784d15a933b7b6335bd9029c46
parinitm <webgility123>
2020-06-16 15:56:30 +0530

    Stage 2/6: Enough coffee for everyone

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,14 +1,22 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
 
-        System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!");
+        System.out.println("Write how many cups of coffee you will need:");
+        Scanner scanner = new Scanner(System.in);
+
+        int coffees = scanner.nextInt();
+        int water = 200 * coffees;
+        int milk = 50 * coffees;
+        int beans = 15 * coffees;
+
+        System.out.println("For " + coffees + " cups of coffee you will need:");
+        System.out.println(water + " ml of water");
+        System.out.println(milk + " ml of milk");
+        System.out.println(beans + " g of coffee beans");
+
     }
 }
